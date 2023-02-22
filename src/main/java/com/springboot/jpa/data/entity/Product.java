ccb2f4bee7a9ac6ced342618ea8a9e01ceeebb52
wisdom <bsbpsj97@naver.com>
2023-02-22 22:23:07 +0900

    데이터베이스 연동

---
+++ src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -0,0 +1,35 @@
+package com.springboot.jpa.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter  
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.jpa.data.repository;
+
+import com.springboot.jpa.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
